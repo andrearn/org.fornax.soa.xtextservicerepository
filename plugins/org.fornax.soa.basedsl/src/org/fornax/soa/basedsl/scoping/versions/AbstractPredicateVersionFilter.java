5c6175fd3e7fa82996dc131d7921207d503018d7
André Arnold <andre.arnold@gmx.de>
2012-06-28 17:53:21 +0200

    allow to set a delegate  Predicate to pre filter all candidates

--- plugins/org.fornax.soa.basedsl/src/org/fornax/soa/basedsl/scoping/versions/AbstractPredicateVersionFilter.java
+++ plugins/org.fornax.soa.basedsl/src/org/fornax/soa/basedsl/scoping/versions/AbstractPredicateVersionFilter.java
@@ -10,11 +10,16 @@ import com.google.common.collect.Multimap;
 
 public abstract class AbstractPredicateVersionFilter<T> implements VersionFilter<T>,
 		Predicate<T> {
+	
+	private Predicate<T> preFilterPredicate;
 
 	public final static AbstractPredicateVersionFilter<IEObjectDescription> NULL_VERSION_FILTER = new NullVersionFilter<IEObjectDescription>();
 
 	public boolean apply(T input) {
-		return true;
+		if (getPreFilterPredicate() != null)
+			return getPreFilterPredicate().apply(input);
+		else
+			return true;
 	}
 	
 	protected IEObjectDescription getCurrentBestMatch (final Multimap<QualifiedName, IEObjectDescription> canditates, final QualifiedName name) {
@@ -23,6 +28,14 @@ public abstract class AbstractPredicateVersionFilter<T> implements VersionFilter
 			return canditates.get (name).iterator().next();
 		}
 		return null;
+	}
+
+	public Predicate<T> getPreFilterPredicate() {
+		return preFilterPredicate;
+	}
+
+	public void setPreFilterPredicate(Predicate<T> preFilterPredicate) {
+		this.preFilterPredicate = preFilterPredicate;
 	}
 
 
