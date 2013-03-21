1a3aaf5ead6141fd236cf2eca7d0c62e2e986aaa
athurn <andreas.thurn@anderScore.com>
2013-03-21 15:24:55 +0100

    fixed testing errors

---
+++ test/org.fornax.soa.test/src/org/fornax/soa/test/DummyTest.java
@@ -0,0 +1,19 @@
+package org.fornax.soa.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    assertTrue(true);
+  }
+
+}
